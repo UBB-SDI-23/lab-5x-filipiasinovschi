d61f9a34bd97257a1daf31adbbbe6fb286547999
filipiasinovschi <72267969+filipiasinovschi@users.noreply.github.com>
2023-04-27 01:22:38 +0300

    e

--- src/interfaces/Author.ts
+++
@@ -1,8 +0,0 @@
-export interface Author {
-    id: number
-    first_name: string
-    last_name:string
-    birth_date: Date
-    email: string
-    website: string
-}
--- src/interfaces/Buyer.ts
+++
@@ -1,5 +0,0 @@
-export interface Buyer {
-    id: number
-    name: string
-    email: string
-}
