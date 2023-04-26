84ce79e7009f610fa406482ec13969afdef8c149
filipiasinovschi <72267969+filipiasinovschi@users.noreply.github.com>
2023-04-27 02:36:05 +0300

    used server be

--- my-app/src/common/constants.ts
+++ my-app/src/common/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL=`ec2-16-171-56-196.eu-north-1.compute.amazonaws.com`
+export const BASE_URL=`http://ec2-16-171-56-196.eu-north-1.compute.amazonaws.com`
 // export const BASE_URL=`http://127.0.0.1:8000`
